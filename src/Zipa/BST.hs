3960abc21091acc815585bf8de4b56adf0f0c0fc
Pedro Veloso <86307207+pedrohaveloso@users.noreply.github.com>
2026-03-01 12:04:47 -0300

    feat: implement binary search tree with basic operations

---
+++ src/Zipa/BST.hs
@@ -0,0 +1,59 @@
+module Zipa.BST
+  ( BST (..),
+    extract,
+    empty,
+    first,
+    from,
+    fromList,
+    insert,
+    insertBy,
+    merge,
+    size,
+  )
+where
+
+data BST a
+  = Empty
+  | Node (BST a) a (BST a)
+  deriving (Show)
+
+empty :: BST a
+empty = Empty
+
+insert :: (Ord a) => a -> BST a -> BST a
+insert value (Node left leaf right)
+  | leaf < value = Node left leaf (insert value right)
+  | otherwise = Node (insert value left) leaf right
+insert value Empty = Node Empty value Empty
+
+insertBy :: (a -> a -> Ordering) -> a -> BST a -> BST a
+insertBy cmp value (Node left leaf right) =
+  case value `cmp` leaf of
+    LT -> Node (insertBy cmp value left) leaf right
+    _ -> Node left leaf (insertBy cmp value right)
+insertBy _ value Empty = Node Empty value Empty
+
+merge :: a -> BST a -> BST a -> BST a
+merge parent a = Node a parent
+
+from :: (Ord a) => a -> BST a
+from = flip insert empty
+
+fromList :: (Ord a) => [a] -> BST a
+fromList = foldl (flip insert) empty
+
+first :: BST a -> Maybe a
+first (Node Empty value _) = Just value
+first (Node left _ _) = first left
+first Empty = Nothing
+
+size :: BST a -> Int
+size Empty = 0
+size (Node left _ right) = 1 + size left + size right
+
+extract :: BST a -> Maybe (a, BST a)
+extract Empty = Nothing
+extract (Node Empty value right) = Just (value, right)
+extract (Node left value right) = do
+  (value', left') <- extract left
+  Just (value', Node left' value right)
